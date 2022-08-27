8492ed24cf9712c0513654c40dbbc23bda104b53
idelblania Almeida <102764800+idelblaniaalmeida@users.noreply.github.com>
2022-08-27 04:16:03 -0300

    meus exercícios para a prova de JS

---
+++ ex04.js
@@ -0,0 +1,4 @@
+let num = [1,2,3,4,'Luidy',6,7,8,'Moura','Luidy',11,12,13,14,'Luidy',16,17,'Moura',19,'Luidy',21,22,23,24,'Luidy',26,'Moura',28,29,'Luidy',31,32,33,34,'Luidy','Moura',37,38,39,'Luidy',41,42,43,44,'LuidyMoura']
+for(let num = 0; num< num.length;num++)   
+num.sort()
+console.log(num)
