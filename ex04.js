3bc2460e99b7ed660e15449c0a9f1213969884eb
idelblania Almeida <102764800+idelblaniaalmeida@users.noreply.github.com>
2022-08-27 21:17:57 -0300

    adaptação

--- ex04.js
+++ ex04.js
@@ -1,4 +1,37 @@
-let num = [1,2,3,4,'Luidy',6,7,8,'Moura','Luidy',11,12,13,14,'Luidy',16,17,'Moura',19,'Luidy',21,22,23,24,'Luidy',26,'Moura',28,29,'Luidy',31,32,33,34,'Luidy','Moura',37,38,39,'Luidy',41,42,43,44,'LuidyMoura']
-for(let num = 0; num< num.length;num++)   
-num.sort()
-console.log(num)
+//demostrativo de teste
+
+const LuidyMoura = (NumeroLimite) => {
+
+    let Resultado = []
+ 
+    for( let i = 1; i <= NumeroLimite; i++){
+ 
+         if(i % 5 == 0){
+ 
+            Resultado.push("Luidy")
+ 
+         }
+ 
+         if(i % 9 == 0){
+ 
+            Resultado.push("Moura")
+ 
+         }
+ 
+         if (i % 5 == 0 && i % 9 == 0){
+ 
+            Resultado.push("LuidyMoura")
+ 
+         }
+ 
+         if (i % 5 != 0 || i % 9 != 0) {
+ 
+            Resultado.push(i)
+ 
+         }
+ 
+    }
+ 
+    return (Resultado)
+ 
+ }
